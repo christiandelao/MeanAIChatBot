6f4c4db86c532b7407fe87c63a7f5abe57a26058
Christian Delao <114203238+christiandelao@users.noreply.github.com>
2024-02-19 22:38:37 -0800

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = <#api key goes here>;
 const systemMessage = {
   "role": "system", 
   "content": "Talk to me very rudely."
